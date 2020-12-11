f1548cdbb965f802f36f37256f8c9e76dfb486c7
lyndon lyu <7yeahslv@gmail.com>
2020-12-11 19:18:11 +0100

    add exercise p15

---
+++ cmake-build-debug/CMakeFiles/practiceC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practiceC.dir/Binary/converting_HEX_to_BIN_situation1.c.obj"
+  "CMakeFiles/practiceC.dir/C_Primer_Plus/first.c.obj"
+  "practiceC.exe"
+  "practiceC.exe.manifest"
+  "practiceC.lib"
+  "practiceC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/practiceC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
